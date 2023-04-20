c89a45ec6c1f0da9880aec947deda348e3cffc92
caru <carola.molina@gmail.com>
2023-04-20 09:49:31 -0300

    ajustes após acertos de tentativas

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        Scanner leitor = new Scanner(System.in);
+
         int aleatorio = new Random().nextInt(100);
 
-        Scanner numUsuario = new Scanner(System.in);
+        int numUsuario;
 
-        System.out.print("Digite um número: ");
+        int tentativas = 0;
 
-        numUsuario =  numUsuario.nextInt();
+        while (tentativas <=5) {
+
+            System.out.print("Digite um número: ");
+
+            numUsuario = leitor.nextInt();
+
+            if (aleatorio == numUsuario) {
+
+                System.out.println("Você acertou o número");
+                break;
+            }
+            System.out.println("Você errou, tente novamente: ");
+            tentativas++;
+
+        }
+
+        if(tentativas == 5){
+            System.out.println("Você não acertou, o número era: " + leitor);
+        }
 
     }
 }
